0ea9dffa5d08337380fd8adfc160419f89bf8c74
acoezard <axel.coezard@gmail.com>
2024-11-19 20:57:13 +0100

    add a check after vma init and a log

--- Sources/Core/src/Graphics/Vulkan/VulkanAllocator.cpp
+++ Sources/Core/src/Graphics/Vulkan/VulkanAllocator.cpp
@@ -10,19 +10,22 @@ namespace BetterThanNothing
 {
 	VulkanAllocator::VulkanAllocator(ApplicationContext* pContext)
 	{
-		 VmaVulkanFunctions vulkanFunctions = {};
-		 vulkanFunctions.vkGetInstanceProcAddr = &vkGetInstanceProcAddr;
-		 vulkanFunctions.vkGetDeviceProcAddr = &vkGetDeviceProcAddr;
+		VmaVulkanFunctions vulkanFunctions = {};
+		vulkanFunctions.vkGetInstanceProcAddr = &vkGetInstanceProcAddr;
+		vulkanFunctions.vkGetDeviceProcAddr = &vkGetDeviceProcAddr;
 
-		 VmaAllocatorCreateInfo allocatorCreateInfo = {};
-		 allocatorCreateInfo.flags = VMA_ALLOCATOR_CREATE_EXT_MEMORY_BUDGET_BIT;
-		 allocatorCreateInfo.vulkanApiVersion = pContext->GetVulkanApiVersion();
-		 allocatorCreateInfo.physicalDevice = pContext->GetVulkanDevice()->PhysicalHandle();
-		 allocatorCreateInfo.device = pContext->GetVulkanDevice()->LogicalHandle();
-		 allocatorCreateInfo.instance = pContext->GetVulkanInstance()->Handle();
-		 allocatorCreateInfo.pVulkanFunctions = &vulkanFunctions;
+		VmaAllocatorCreateInfo allocatorCreateInfo = {};
+		allocatorCreateInfo.flags = VMA_ALLOCATOR_CREATE_EXT_MEMORY_BUDGET_BIT;
+		allocatorCreateInfo.vulkanApiVersion = pContext->GetVulkanApiVersion();
+		allocatorCreateInfo.physicalDevice = pContext->GetVulkanDevice()->PhysicalHandle();
+		allocatorCreateInfo.device = pContext->GetVulkanDevice()->LogicalHandle();
+		allocatorCreateInfo.instance = pContext->GetVulkanInstance()->Handle();
+		allocatorCreateInfo.pVulkanFunctions = &vulkanFunctions;
 
-		 vmaCreateAllocator(&allocatorCreateInfo, &m_allocator);
+		if (vmaCreateAllocator(&allocatorCreateInfo, &m_allocator) != VK_SUCCESS)
+			throw std::runtime_error("Failed to create Vulkan allocator");
+
+		LOG_SUCCESS("Vulkan memory allocator (Vma): ok");
 	}
 
 	VulkanAllocator::~VulkanAllocator()
