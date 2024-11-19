7e2c9673464805890626d48e401563a2471f06bb
axelcoezard <axel.coezard@gmail.com>
2024-11-19 13:46:25 +0100

    cleaned Application.hpp

--- Sources/Core/src/Application.hpp
+++ Sources/Core/src/Application.hpp
@@ -6,34 +6,19 @@ namespace BetterThanNothing
 {
 	class ApplicationContext;
 
-	/**
-	 * @brief The Application class is the main class of the engine's applications.
-	 */
 	class Application
 	{
 	protected:
-		/**
-		 * @brief A pointer to the Window
-		*/
 		Window m_window;
+
 	public:
 
 		Application() = default;
 		virtual ~Application() = default;
 
-		/**
-		 * @brief Run the Application
-		*/
 		void Run();
 
-		/**
-		 * @brief Called when the Application is enabled
-		*/
 		virtual void OnEnable(ApplicationContext* context) = 0;
-
-		/**
-		 * @brief Called when the Application is disabled
-		*/
 		virtual void OnDisable(ApplicationContext* context) = 0;
 	};
 };
